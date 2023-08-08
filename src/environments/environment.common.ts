3bed84f4ce7c50dab668e5fede97af2f724d68ba
MicaCblls <micaelaceballos411@gmail.com>
2023-08-07 23:03:15 -0300

    add process

--- src/environments/environment.common.ts
+++ src/environments/environment.common.ts
@@ -1,4 +1,4 @@
 export const commonEnvironment = {
-  apiKey: import.meta.env['NG_APP_API'],
-  apiUrl: import.meta.env['VERCEL_APP_URL'],
+  apiKey: process.env.NG_APP_API,
+  apiUrl: process.env.VERCEL_APP_URL,
 };
